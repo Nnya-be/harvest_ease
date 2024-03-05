041ec7c79a60d40e2879e1d2159b7d97ea62a182
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-03-05 04:49:01 +0000

    Added: dependencies for tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
+  plugins: [],
+}
+
