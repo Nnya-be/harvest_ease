bffa20d7e107dc9fc62ea7d1d671920ef24e83fc
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-03-06 04:14:13 +0000

    Main miports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import Navigation from "./components/Navigation";
+import AboutUs from "./components/AboutUs";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <div className="bg-gradient-to-bl from-slate-500/10 to-green-700/20 h-lvh">
-        <Navigation></Navigation>
+      <div>
+        <Header></Header>
+        <AboutUs></AboutUs>
       </div>
     </div>
   );
