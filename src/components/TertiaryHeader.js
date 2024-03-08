eddd64652c036ccb5cdd69c2560e028442b95ebc
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-03-08 01:33:10 +0000

    Untouched:

--- src/components/TertiaryHeader.js
+++ src/components/TertiaryHeader.js
@@ -3,9 +3,9 @@ import React from 'react'
 function TertiaryHeader(props) {
     const heading = props.heading;
   return (
-    <>
-        <h3 className='text-7xl text-start font-bold border-b-4 border-green-500 flex divide-x-4'>{heading}</h3>
-    </>
+    <div>
+        <h3 className='text-7xl text-start font-bold  flex divide-x-4'>{heading}</h3>
+    </div>
   )
 }
 
