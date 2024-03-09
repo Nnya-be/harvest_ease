37fffb80d5330d50e49b51caba8721edd954fdb3
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-03-09 04:39:15 +0000

    Button components

---
+++ src/components/ReadMoreButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ReadMoreButton() {
+  return (
+    <>
+    <button className='text-xl  border-2  border-green-600 hover:bg-green-600 px-6 py-4 rounded-lg hover:text-emerald-300 capitalize'> read more </button></>
+  )
+}
+
+export default ReadMoreButton
