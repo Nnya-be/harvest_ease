a3bf1fe79c591bdf28d5aef717b2d347590bb067
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-03-09 04:44:14 +0000

    Added: border radius

--- src/components/ChooseCards.js
+++ src/components/ChooseCards.js
@@ -6,7 +6,7 @@ import SectionParagraph from './SectionParagraph'
 function ChooseCards(props) {
     const card_details = props.details
   return (
-    <div className='w-72 h-96 border-green-600 border-2 flex flex-col justify-around hover:bg-green-600 hover:text-slate-400 items-center p-2'>
+    <div className='w-72 h-96 border-green-600 border-2 flex flex-col justify-around hover:bg-green-600 hover:text-slate-400 items-center p-2 rounded-lg'>
         <img src={card_details.icon} className='w-28'></img>
         <h4 className='text-slate-700 text-2xl'>{card_details.heading}</h4>
         <SectionParagraph paragraph={card_details.paragraph}></SectionParagraph>
