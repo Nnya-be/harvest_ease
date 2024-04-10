52668689cc2c33d8bedca218b3ce11bae6d275df
Futuristic-Dev <papaamoah568@gmail.com>
2024-04-10 14:50:41 +0000

    Update image opacity in Header component

--- src/components/TertiaryHeader.js
+++ src/components/TertiaryHeader.js
@@ -4,7 +4,7 @@ function TertiaryHeader(props) {
     const heading = props.heading;
   return (
     <div>
-        <h3 className='md:text-4xl xl:text-7xl text-start font-bold  flex divide-x-4'>{heading}</h3>
+        <h3 className='md:text-4xl xl:text-7xl text-start font-bold  flex divide-x-4'>{  heading }</h3>
     </div>
   )
 }
